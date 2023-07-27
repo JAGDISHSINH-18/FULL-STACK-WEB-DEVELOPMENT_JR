147c7c8bf7667f47ba9a05da884c478b53b5020d
JAGDISHSINH-18 <96286631+JAGDISHSINH-18@users.noreply.github.com>
2023-07-27 10:43:22 +0530

    Delete Week-3 directory

--- Week-3/Task-2.js
+++
@@ -1,7 +0,0 @@
-// Importing module
-import chalk from 'chalk';
-
-// Printing the text
-console.log(chalk.yellow("21IT135"))
-console.log(chalk.blue.underline("Jagdish"))
-console.log(chalk.green.underline.bold("GFG"))
